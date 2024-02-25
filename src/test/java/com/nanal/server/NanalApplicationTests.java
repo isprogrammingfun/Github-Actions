91337e770fff43eedaa58986473d4a4f68994072
seeunjang <wisjang1@naver.com>
2024-02-25 20:27:40 +0900

    refactor : Test 어노테이션 주석 제거

--- src/test/java/com/nanal/server/NanalApplicationTests.java
+++ src/test/java/com/nanal/server/NanalApplicationTests.java
@@ -3,7 +3,7 @@ package com.nanal.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class NanalApplicationTests {
 
     @Test
